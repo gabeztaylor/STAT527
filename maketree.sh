70cfc63c349c675b799eceb72d3fae78f7445355
Gabe Taylor <gabet2@illinois.edu>
2021-11-24 13:39:34 -0600

    tree visualization

--- maketree.sh
+++ maketree.sh
@@ -2,6 +2,8 @@
 
 cd ~/Downloads
 cd h2o-3.34.0.4
-java -cp h2o.jar hex.genmodel.tools.PrintMojo --tree 0 -i "/Users/gabrieltaylor/Python/STAT527/model.zip" -o model.gv -f 20 -d 3
-dot -Tpng model.gv -o /Users/gabrieltaylor/Python/STAT527/imgs/model.png
-open model.png
+
+
+java -cp h2o.jar hex.genmodel.tools.PrintMojo --tree 0 -i "/Users/gabrieltaylor/Python/STAT527/DTmodels/model$1.zip" -o model.gv -f 20 -d 3
+dot -Tpng model.gv -o /Users/gabrieltaylor/Python/STAT527/imgs/model$1.png
+#open /Users/gabrieltaylor/Python/STAT527/imgs/model$1.png
